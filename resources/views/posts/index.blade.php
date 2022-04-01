edd3cae25a523ae2f1702b2a7734a25356178caf
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-01 23:56:52 +0100

    changed the title, and made it so only the posts are shown, as create is now accessed a different way

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,12 +3,14 @@
 @section('title','Main Feed')
 
 @section('content')
-    <a href="{{route('posts.create')}}">Create Post</a>
-
-    <p> Posts from other users </p>
     <ul>
         @foreach($posts as $post)
             <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
         @endforeach
     </ul>
 @endsection
+
+
+
+
+
