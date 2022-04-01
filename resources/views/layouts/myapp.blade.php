18b9b8a43fa689044427120faa0c02bb87a1ccfb
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-01 23:58:14 +0100

    changed the style to an x-app layout, and changed how the titles are displayed

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -1,33 +1,47 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Swansea Zoo -
-        @yield('title')</title>
-</head>
-<body>
-<h1>Catagram - @yield('title')</h1>
-@if($errors->any())
-    <div>
-        Errors:
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
+<x-app-layout>
+    <head>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                @yield('title')
+            </h2>
+        </x-slot>
+
+
+    </head>
+
+
+    <body>
+    @if($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @error('name')
+    <div class="alert alert-danger">
+        {{$message}}
     </div>
-@endif
-@error('name')
-<div class="alert alert-danger">
-    {{$message}}
-</div>
-@enderror
+    @enderror
 
-@if(session('message'))
-    <p><b>{{session('message')}}</b></p>
-@endif
+    @if(session('message'))
+        <p><b>{{session('message')}}</b></p>
+    @endif
 
-<div>
-    @yield('content')
-</div>
-</body>
-</html>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </body>
+</x-app-layout>
