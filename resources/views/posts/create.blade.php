e41f90202f45785cdb599f634b43986f17f3984c
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-04 11:37:35 +0100

    added the old captions option within creating a post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,7 +7,6 @@
         @csrf
         <p>Caption: <input type="text" name="caption" value="{{old('caption')}}"></p>
         <p>Image: <input type="text" name="image_filepath" value="{{old('image_filepath')}}"></p>
-        <p>User ID: <input type="text" name="user_id" value="{{old('user_id')}}"></p>
         <input type="submit" value="Submit">
         <a href="{{route('posts.index')}}">Cancel</a>
     </form>
