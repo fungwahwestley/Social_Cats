89699bcd324507bfa60cc0b0584dd30fd726d6f1
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-05 11:00:35 +0100

    now has a section where you can create a comment and where you can see comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,11 +11,25 @@
           action="{{route('posts.destroy', ['post'=>$post])}}">
         @csrf
         @method('DELETE')
-        <button type="submit">Delete</button>
+        <button type="submit">Delete Post</button>
     </form>
 
-    <a href="{{route('posts.edit',['post'=>$post])}}">Edit</a>
+    <a href="{{route('posts.edit',['post'=>$post])}}">Edit Post</a>
 
     <a href="{{route('posts.index')}}">Back</a>
+
+    <form method="POST" action="{{route('comments.store',['post'=>$post])}}">
+        @csrf
+        <p>Add Comment: <input type="text" name="content" value="{{old('content')}}"></p>
+        <input type="submit" value="Submit">
+        <a href="{{route('posts.show',['post'=>$post])}}">Cancel</a>
+    </form>
+
+    <ul>
+        <p> Comments: </p>
+        @foreach($comments as $comment)
+            <li>{{$comment->content}} by {{$comment->user->name}}</li>
+        @endforeach
+    </ul>
 
 @endsection
