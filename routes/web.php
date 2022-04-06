c7c26f8828546380e593dd0417e869a78bf09e18
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-06 19:10:38 +0100

    middleware group function has been applied to all my routes so the user has to be login in to access them

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Post routes
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
-Route::post('/posts',[PostController::class,'store'])->name('posts.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-
-//comment routes
-Route::post('/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
-//Route::delete('/posts/{post}', [CommentController::class, 'destroy'])->name('comments.destroy');
-//Route::get('/posts{post}/edit', [CommentController::class, 'edit'])->name('comments.edit');
-//Route::post('/posts/{post}', [CommentController::class, 'update'])->name('comments.update');
-
-//profile routes
-Route::get('/profile',[ProfileController::class, 'index'])->name('profile.index');
-
 Route::get('/example', [PostController::class,'example']);
 
 Route::middleware(['auth'])->group(function(){
     Route::view('/admin','admin')->name('admin');
+
+    //Post routes
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+    Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
+//comment routes
+    Route::post('/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{comment}', [CommentController::class, 'show'])->name('comments.show');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::post('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+
+//profile routes
+    Route::get('/profile',[ProfileController::class, 'index'])->name('profile.index');
 });
 
 
