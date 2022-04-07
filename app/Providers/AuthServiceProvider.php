8204e08163feb5abd867fabab212a165fabe3660
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-07 19:23:17 +0100

    Used gated for admin and user rules. This means only admins can edit everything and users can edit only their own posts

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Providers\Response;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -28,19 +29,45 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Users can only edit and delete their own posts
         Gate::define('update-post', function (User $user, Post $post) {
             return $user->id === $post->user_id;
+
         });
+
         Gate::define('delete-post', function (User $user, Post $post) {
             return $user->id === $post->user_id;
+
         });
 
         Gate::define('update-comment', function (User $user, Comment $comment) {
             return $user->id === $comment->user_id;
+
         });
         Gate::define('delete-comment', function (User $user, Comment $comment) {
             return $user->id === $comment->user_id;
+
         });
+
+        //Auth can edit and delete all posts
+        Gate::define('update-post-admin', function (User $user) {
+            return $user->admin === 1;
+
+        });
+
+        Gate::define('delete-post-admin', function (User $user) {
+            return $user->admin === 1;
+
+        });
+
+        Gate::define('update-comment-admin', function (User $user) {
+            return $user->admin === 1;
+
+        });
+        Gate::define('delete-comment-admin', function (User $user) {
+            return $user->admin === 1;
+
+        });
+
     }
 }
