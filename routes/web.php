5c0063fa25406b28ca654fc90d15ffb037263181
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-08 12:09:24 +0100

    has new routes for ajax commenting

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CommentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,21 +20,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/example', [PostController::class,'example']);
+Route::get('/example', [PostController::class, 'example']);
 
-Route::middleware(['auth'])->group(function(){
-    Route::view('/admin','admin')->name('admin');
+Route::middleware(['auth'])->group(function () {
+    Route::view('/admin', 'admin')->name('admin');
 
     //Post routes
     Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
-    Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
     Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
     Route::get('/posts{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
 //comment routes
+    Route::get('/posts/{post}', [CommentController::class, 'page'])->name('posts.show-api');
     Route::post('/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
     Route::get('/comments/{comment}', [CommentController::class, 'show'])->name('comments.show');
     Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
@@ -41,7 +43,7 @@ Route::middleware(['auth'])->group(function(){
     Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
 
 //profile routes
-    Route::get('/profile',[ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
 });
 
 
@@ -49,4 +51,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
