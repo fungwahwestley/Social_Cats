230ae09929a528ee5e89993d09103552e68f7016
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-17 17:39:50 +0100

    added likes into each post displayed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,8 @@
 @section('content')
     <ul>
         @foreach($posts as $post)
-            <li><a href="{{route('posts.show-api', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
+            <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
+            <li>Likes: {{$post->likes()->count()}} | Comments: {{$post->comments()->count()}}<br/><br/></li>
         @endforeach
     </ul>
 @endsection
