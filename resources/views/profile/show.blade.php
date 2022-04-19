8944cce8ee3db3a4685cfeaaa8765af58a2f0fd9
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-19 19:52:07 +0100

    shows a users profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -6,7 +6,8 @@
 
 @section('content')
 
-    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{route('posts.index')}}"> Back to Main Feed<br><br></a>
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{route('posts.index')}}"> Back to Main
+        Feed<br><br></a>
 
     <ul>
         <li>Name: {{$user->name}}</li>
@@ -16,13 +17,15 @@
     <br/><br/>
     <p class="font-semibold text-lg">Posts: </p>
     <ul>
-    @foreach($user->posts()->get() as $post)
-        <!-- <img src="{{ url('public/image/'.$post->path) }}"
-                 style="height: 100px; width: 150px;">-->
-        <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
-        <li>Likes: {{$post->likes()->count()}} | Comments: {{$post->comments()->count()}}<br/><br/></li>
+        @foreach($user->posts()->get() as $post)
+            @if($post->path != null)
+                <img src="{{ url($post->path) }}"
+                     style="height: 100px; width: 150px;">
+            @endif
+            <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
+            <li>Likes: {{$post->likes()->count()}} | Comments: {{$post->comments()->count()}}<br/><br/></li>
 
-    @endforeach
+        @endforeach
     </ul>
 
 
