690a97b42e747d6c549b499925bef433273835cb
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-19 15:49:34 +0100

    when linking into a user in posts.show, whether it be comments/posts, this page is shown

---
+++ resources/views/profile/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.myapp')
+
+@section('title','Profile')
+
+
+
+@section('content')
+
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{route('posts.index')}}"> Back to Main Feed<br><br></a>
+
+    <ul>
+        <li>Name: {{$user->name}}</li>
+        <li>Email: {{$user->email}}</li>
+    </ul>
+
+    <br/><br/>
+    <p class="font-semibold text-lg">Posts: </p>
+    <ul>
+    @foreach($user->posts()->get() as $post)
+        <!-- <img src="{{ url('public/image/'.$post->path) }}"
+                 style="height: 100px; width: 150px;">-->
+        <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
+        <li>Likes: {{$post->likes()->count()}} | Comments: {{$post->comments()->count()}}<br/><br/></li>
+
+    @endforeach
+    </ul>
+
+
+@endsection
