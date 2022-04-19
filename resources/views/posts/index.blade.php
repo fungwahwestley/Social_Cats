d1e1517c000b29a5cd909c73dcb8629bfe37183a
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-19 19:45:32 +0100

    if an image if the post has an image

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,10 @@
 @section('content')
     <ul>
         @foreach($posts as $post)
+            @if($post->path != null)
+                <img src="{{ url('public/images'.$post->path) }}"
+                     style="height: 100px; width: 150px;">
+            @endif
             <li><a href="{{route('posts.show', ['post'=>$post])}}">{{$post->caption}} by {{$post->user->name}}</a></li>
             <li>Likes: {{$post->likes()->count()}} | Comments: {{$post->comments()->count()}}<br/><br/></li>
         @endforeach
