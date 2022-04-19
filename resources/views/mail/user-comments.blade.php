de1493a3996088060ffb816f74f5eeeb6d32aa27
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-19 22:43:27 +0100

    creating notifications for when users comments on a post, sends an email to them

---
+++ resources/views/mail/user-comments.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+    # Notification Alert
+
+    {{ $comment->user->name }} has commented on your post!
+
+
+
+
+    See you soon,
+     Catagram Team ᓚᘏᗢ
+@endcomponent
