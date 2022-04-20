8cbc1cbfb3261653faa9f97052558d1ca216db3b
FungWah <74245673+fungwahwestley@users.noreply.github.com>
2022-04-20 17:38:36 +0100

    changed routes so i could work on api routes

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    //Route::get('/posts/{post}', [CommentController::class, 'page'])->name('posts-api.show');
     Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-    // Route::get('/posts/{post}', [CommentController::class, 'page'])->name('posts-api.show');
     Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
     Route::get('/posts{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
@@ -47,7 +47,6 @@ Route::middleware(['auth'])->group(function () {
 
 
     //comment routes
-    //  Route::get('/posts/{post}', [CommentController::class, 'page'])->name('posts-api.show');
     Route::post('/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
     Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
     Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
